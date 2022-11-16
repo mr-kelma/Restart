6ca9cd57a3bdd9b165c8b597a0135d5b42ce575d
mr-kelma <mr.kelma@gmail.com>
2022-11-16 19:43:33 +0400

    Update Onboarding screen

--- Restart/OnboardingView.swift
+++ Restart/OnboardingView.swift
@@ -46,13 +46,75 @@ struct OnboardingView: View {
                 ZStack {
                     ZStack {
                         Circle()
-                            .stroke(.white.opacity(0.2))
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
                     } //: ZSTACK
+                    
+                    Image("characterOne")
+                        .resizable()
+                        .scaledToFit()
                 } //: CENTER
                 
                 Spacer()
                 
                 // MARK: - FOOTER
+                
+                ZStack {
+                    // PARTS OF THE CUSTOM BUTTON
+                    
+                    // 1. BACKGROUND (STATIC)
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.3))
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    
+                    // 2. CALL-TO-ACTION (STATIC)
+                    
+                    Text("Get Started!")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x:20)
+                    
+                    // 3. CAPSULE (DYNAMIC WIDTH)
+                    
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    
+                    // 4. CIRCLE (DRAGGABLE)
+                    
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 25, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment:  .center)
+                        .onTapGesture {
+                            isOnboardingViewActive = false
+                        }
+                        
+                        Spacer()
+                    }
+                } //: FOOTER
+                .frame(width: 300, height: 80, alignment: .center)
+                .padding()
             } //: VSTACK
         } //: ZSTACK
     }
