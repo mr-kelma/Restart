4f06772fbbe4ec1f6371371b7c66790d6e1c7118
mr-kelma <mr.kelma@gmail.com>
2022-11-16 17:35:11 +0400

    Setting up an Onboarding screen

--- Restart/OnboardingView.swift
+++ Restart/OnboardingView.swift
@@ -8,8 +8,53 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    // MARK: PROPERTY
+
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    // MARK: - BODY
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            
+            VStack(spacing: 20) {
+                // MARK: - HEADER
+                
+                Spacer()
+                
+                VStack(spacing: 0) {
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                    It's not how much we give but
+                    how much love we put into giving.
+                    """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                } //: HEADER
+                
+                // MARK: - CENTER
+                
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2))
+                    } //: ZSTACK
+                } //: CENTER
+                
+                Spacer()
+                
+                // MARK: - FOOTER
+            } //: VSTACK
+        } //: ZSTACK
     }
 }
 
